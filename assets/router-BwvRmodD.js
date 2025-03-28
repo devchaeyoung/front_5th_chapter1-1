5d41a1c5457fca8ed48cc45ccea41e7b59ac8305
devchaeyoung <chaemaa00@gmail.com>
2025-03-28 11:10:39 +0900

    Updates

--- assets/router-BwvRmodD.js
+++ assets/router-BwvRmodD.js
@@ -27,12 +27,12 @@
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class=${d("/")?"text-blue-600 font-bold":"text-gray-600"}>홈</a></li>
+          <li><a href="/" class="${d("/")?"text-blue-600 font-bold":"text-gray-600"}">홈</a></li>
         ${t?`
-          <li><a href="/profile" class=${d("/profile")?"text-blue-600 font-bold":"text-gray-600"}>프로필</a></li>
+          <li><a href="/profile" class="${d("/profile")?"text-blue-600 font-bold":"text-gray-600"}">프로필</a></li>
           <li id="logout"><a href="#" class="text-gray-600">로그아웃</a></li>
           `:`
-          <li><a href="/login" class=${d("/login")?"text-blue-600 font-bold":"text-gray-600"}>로그인</a></li>
+          <li><a href="/login" class="${d("/login")?"text-blue-600 font-bold":"text-gray-600"}">로그인</a></li>
           `}
         </ul>
       </nav>
